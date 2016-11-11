d9f6b363112c5e8d9a3fce4886fe13421c9848bc
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2016-11-11 23:21:01 +0100

    added initial bot.py

---
+++ gatekeeper/bot.py
@@ -0,0 +1,41 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+def start(bot, update):
+    update.message.reply_text('Hello World!')
+
+def hello(bot, update):
+    update.message.reply_text(
+        'Hello {}'.format(update.message.from_user.first_name))
+
+def echo(bot, update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=update.message.text)
+
+def caps(bot, update, args):
+    text_caps = ' '.join(args).upper()
+    bot.sendMessage(chat_id=update.message.chat_id, text=text_caps)
+
+def addNewUser(bot, update):
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Are you a boy or a girl?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return ''
+
+updater = Updater('293092231:AAEVHlXq0RmYk1Dmnw43DhRyksudxPWd9YE')
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('add', addNewUser))
+
+echo_handler = MessageHandler(Filters.text, echo)
+caps_handler = CommandHandler('caps', caps, pass_args=True)
+
+updater.dispatcher.add_handler(echo_handler)
+updater.dispatcher.add_handler(caps_handler)
+
+updater.start_polling()
+updater.idle()
