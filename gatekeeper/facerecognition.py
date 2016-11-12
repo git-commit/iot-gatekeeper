a69ad973a4c87f8b806493fc5a988208086cb02f
Alex Melkonyan <sasha.melkonyan@gmail.com>
2016-11-12 01:21:19 +0100

    Implement person verification

--- gatekeeper/facerecognition.py
+++ gatekeeper/facerecognition.py
@@ -16,11 +16,14 @@ class FaceRecognition:
             face_id, last_decode = decoded_auth_faces[image_name]
             if self.is_valid(last_decode):
                 return face_id
-        face_id = self.decode_face(image_name)
+        face_id = self.decode_face_from_file(image_name)
         decoded_auth_faces[image_name] = (face_id, datetime.now())
         return face_id
 
-    def decode_face(self, image_name):
+    def decode_face_from_file(self, image_name):
+        return self.decode_page_from_image(open(self.image_folder + image_name, 'rb').read())
+
+    def decode_page_from_image(self, image):
         headers = {
             'Content-Type': 'application/octet-stream',
             'Ocp-Apim-Subscription-Key': '%s' % config.azure_subs_id,
@@ -30,7 +33,6 @@ class FaceRecognition:
             'returnFaceId': 'true',
             'returnFaceLandmarks': 'false'
         }
-        body = open(self.image_folder + image_name, 'rb').read()
         response = requests.post("%s/face/v1.0/detect" % config.azure_api_url, params=params, headers=headers, data=body)
         logging.debug(response.text)
         response.raise_for_status()
@@ -53,8 +55,8 @@ class FaceRecognition:
     def verify_face_id(self, face_id):
         for image_name in os.listdir(self.image_folder):
             if self.are_same(face_id, self.get_face_id(image_name)):
-                return True
-        return False
+                return image_name
+        return None
 
     def is_valid(self, last_decode):
         return datetime.now() - timedelta(days=1) < last_decode
@@ -63,4 +65,5 @@ class FaceRecognition:
         image.download(self.image_folder + '%s.jpg' % name)
 
     def verify_face(self, image):
-        pass
+        face_id = self.decode_page_from_image(image)
+        return self.verify_face_id(face_id)
