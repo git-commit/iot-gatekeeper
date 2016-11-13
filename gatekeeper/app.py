bc85acb34b28a290281aa2c086310e1bda2621ea
yuriyarabskyy <yuriy.arabskyy@gmail.com>
2016-11-13 02:52:35 +0100

    take snaps

--- gatekeeper/app.py
+++ gatekeeper/app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 
 import bot as chat_bot
-# from intercom import Intercom
+from intercom import Intercom
 import logging
 from time import sleep
-# icom = Intercom()
+icom = Intercom()
 
-# def onBellPressed():
-    # if chat_bot.chat_id is None:
-        # logging.warning('Bell is pressed but we have now user in the chat')
-    # chat_bot.verify_image(chat_bot.updater, icom.takePicture())
+def onBellPressed():
+    if chat_bot.chat_id is None:
+        logging.warning('Bell is pressed but we have now user in the chat')
+    chat_bot.verify_image(chat_bot.updater, icom.takePicture())
 
 chat_bot.run_bot()
-# icom.registerOnBellPressedListener(onBellPressed)
+icom.registerOnBellPressedListener(onBellPressed)
+chat_bot.registerOnSnapButtonListener(onBellPressed)
