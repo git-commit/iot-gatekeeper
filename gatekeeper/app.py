82de424723e44a54bd23c4b38dfac5fde24998de
Maximilian Berger <snowdragon92@gmail.com>
2017-01-11 04:55:56 +0100

    Only try to verify a user when a chat_id is set.

--- gatekeeper/app.py
+++ gatekeeper/app.py
@@ -21,7 +21,8 @@ if platform == "linux" or platform == "linux2":
 def onBellPressed():
     if chat_bot.chat_id is None:
         logging.warning('Bell is pressed but we have no user in the chat')
-    chat_bot.verify_image(chat_bot.updater, icom.takePicture())
+    else:
+        chat_bot.verify_image(chat_bot.updater, icom.takePicture())
 
 def onTakeSnap():
     pic = icom.takePicture()
