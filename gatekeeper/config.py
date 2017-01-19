fdaf3c6148f28456e0b641d641f7ebb8fdf21811
Maximilian Berger <snowdragon92@gmail.com>
2017-01-19 14:28:49 +0100

    Update Azure API endpoint to the current address

--- gatekeeper/config.py
+++ gatekeeper/config.py
@@ -1,3 +1,3 @@
-azure_api_url = 'https://api.projectoxford.ai'
+azure_api_url = 'https://westus.api.cognitive.microsoft.com'
 
 test_trump_image_url = 'https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAVYAAAAJDc1ZDE5ODQwLWZkNTktNDQ2Mi04M2Y3LWE2ZWMzZDhjZmYyMQ.jpg'
